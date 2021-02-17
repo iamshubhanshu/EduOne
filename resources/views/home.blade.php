37b42a7793bc4c02e23d0aa8f0e34739e9d550e4
utopian <shubhanshumaur2@gmail.com>
2021-02-17 23:02:17 +0530

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EduOne</title>
+</head>
+<body>
+    <div class="container">
+        <div class="nav">ghu</div>
+    </div>
+</body>
+</html>
